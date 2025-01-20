b52b16dc0c5a66a144483830083b418f44b15963
nusrat52 <nusretibragimli@gmail.com>
2025-01-20 23:25:34 +0400

    postgrsqlWithDocker

--- first_django/settings.py
+++ first_django/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -74,15 +74,41 @@ WSGI_APPLICATION = 'first_django.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
  
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.postgresql",
+#         ""
+#         "OPTIONS": {
+#             "service": "my_service",
+#             "passfile": ".my_pgpass",
+#         },
+#     }
+# }
+ 
+
+
+
+
+
+DATABASES= {
+"default": {
+"ENGINE": "django.db.backends.postgresql",
+"NAME": os.environ.get("SQL_NAME"),
+"USER": os.environ.get("SQL_USER"),
+"PASSWORD": os.environ.get("SQL_PASSWORD"),
+"HOST": os.environ.get("HOST"),
+"PORT": os.environ.get("PORT"),
+}
+}
 
  
 
