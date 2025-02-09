16bec8778d12e1168fd009fb8a0215103b4f855a
nusrat52 <nusretibragimli@gmail.com>
2025-02-09 22:56:51 +0400

    ssl

--- first_django/settings.py
+++ first_django/settings.py
@@ -75,10 +75,10 @@ SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
 
 
 
-CSRF_TRUSTED_ORIGINS = os.environ.get("CSRF_TRUSTED_ORIGINS").split(" ")
+# CSRF_TRUSTED_ORIGINS = os.environ.get("CSRF_TRUSTED_ORIGINS").split(" ")
 
-
-
+CSRF_TRUSTED_ORIGINS = "https://nusretibrahimli.life".split(" ")
+ 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
